5bf8b228d021d151c18719fc3a40b9d8c9640a56
Rituraj Ingale <ingalerd@gmail.com>
2019-10-10 08:46:11 +0530

    Better Animation

--- play.py
+++ play.py
@@ -78,3 +78,59 @@ def decode_time(msg):
 tm = decode_time(msg)
 print(tm)
 """
+from graphics  import *
+from time import gmtime
+
+def initiate(id):
+    win = GraphWin("Window id : "+str(id),400,550)
+    return win
+
+def animate(win):
+    cir = Circle(Point(425,225),25)
+    cir.setFill("#000000")
+    cir.setOutline("#000000")
+    cir.draw(win)
+    n = 425
+    while n > (-30):
+        for i in range(1,98000):
+            continue
+        cir.move(-1,0)
+        n -= 1
+
+    for i in range(1,98000):
+            continue
+
+    cir = Circle(Point(425,225),25)
+    cir.setFill("#d11800")
+    cir.setOutline("#000000")
+    cir.draw(win)
+    n = 425
+    while n > (-30):
+        for i in range(1,98000):
+            continue
+        cir.move(-1,0)
+        n -= 1
+        
+    for i in range(1,98000):
+            continue
+
+    cir = Circle(Point(425,225),25)
+    cir.setFill("#000000")
+    cir.setOutline("#000000")
+    cir.draw(win)
+    n = 425
+    while n > (-30):
+        for i in range(1,98000):
+            continue
+        cir.move(-1,0)
+        n -= 1
+    win.close()
+    return
+
+win = initiate(2)
+s_t=gmtime()
+s_t = (s_t[4],s_t[5])
+animate(win)
+e_t=gmtime()
+e_t = (e_t[4],e_t[5])
+print(str(s_t)+" "+str(e_t))
